17ef8ad309aa5a445a4029d6adfabd75810c96fe
mahutt <tommy.mahut@gmail.com>
2025-10-05 17:55:06 -0400

    Init shadcn

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { Button } from '~/components/ui/button'
 import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
@@ -8,5 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>hello world</main>
+  return (
+    <main>
+      <Button>Hello world</Button>
+    </main>
+  )
 }
