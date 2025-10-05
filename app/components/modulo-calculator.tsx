796bbc8eb251a3a2c8122eba0d2e1f497bb7bc5b
mahutt <tommy.mahut@gmail.com>
2025-10-05 18:25:31 -0400

    Improve modulo calculator

--- app/components/modulo-calculator.tsx
+++ app/components/modulo-calculator.tsx
@@ -2,9 +2,20 @@ import { useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
 
 export default function ModuloCalculator() {
-  const [leftOperand, setLeftOperand] = useState(1)
-  const [rightOperand, setRightOperand] = useState(2)
-  const [result, setResult] = useState(1 % 2)
+  const [leftOperand, setLeftOperand] = useState('1')
+  const [rightOperand, setRightOperand] = useState('1')
+  const [remainder, setRemainder] = useState('0')
+
+  const calculateRemainder = () => {
+    const left = Number(leftOperand)
+    const right = Number(rightOperand)
+    if (!isNaN(left) && !isNaN(right) && right !== 0) {
+      setRemainder((left % right).toString())
+    } else {
+      setRemainder('NaN')
+    }
+  }
+
   return (
     <Card>
       <CardHeader>
@@ -15,26 +26,26 @@ export default function ModuloCalculator() {
           <input
             value={leftOperand}
             onChange={(e) => {
-              const newLeftOperand = Number(e.target.value)
-              setLeftOperand(newLeftOperand)
-              setResult(newLeftOperand % rightOperand)
+              setLeftOperand(e.target.value)
+              calculateRemainder()
             }}
             className="w-16 text-center"
+            onClick={() => setLeftOperand('')}
           />
           <p className="text-gray-500">mod</p>
 
           <input
             value={rightOperand}
             onChange={(e) => {
-              const newRightOperand = Number(e.target.value)
-              setRightOperand(newRightOperand)
-              setResult(leftOperand % newRightOperand)
+              setRightOperand(e.target.value)
+              calculateRemainder()
             }}
             className="w-16 text-center"
+            onClick={() => setRightOperand('')}
           />
 
           <p className="text-gray-500">=</p>
-          <p className="text-gray-500">{result}</p>
+          <p className="text-gray-500">{remainder}</p>
         </div>
       </CardContent>
     </Card>
