f0724bf78349dde04a089e90ba84f181273c8b70
mahutt <tommy.mahut@gmail.com>
2025-10-13 19:41:24 -0400

    Randomize problem order

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,7 @@ import ProblemBox from '~/components/problem-box'
 import type { Route } from './+types/home'
 import ModuloCalculator from '~/components/modulo-calculator'
 import { ProblemType } from '~/types'
+import { useState } from 'react'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,11 +12,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  // Randomize order of ProblemType enum
+  const [problemTypes, setProblemTypes] = useState<ProblemType[]>(
+    Object.values(ProblemType).sort(() => Math.random() - 0.5)
+  )
+
   return (
     <main className="flex max-w-3xl flex-col items-center justify-center gap-4 p-8 mx-auto">
       <ModuloCalculator />
-      <ProblemBox problemType={ProblemType.SquareAndMultiply} />
-      <ProblemBox problemType={ProblemType.ChineseRemainderTheorem} />
+      {problemTypes.map((type) => (
+        <ProblemBox key={type} problemType={type} />
+      ))}
     </main>
   )
 }
